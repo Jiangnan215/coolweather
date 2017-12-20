a723e127d7514a759944585892bba0183943972c
jiang <13135672159@163.com>
2017-12-20 17:17:52 +0800

    加入显示天气信息的功能。。

--- app/src/main/java/com/example/administrator/coolweather/ChooseAreaFragment.java
+++ app/src/main/java/com/example/administrator/coolweather/ChooseAreaFragment.java
@@ -2,6 +2,7 @@ package com.example.administrator.coolweather;
 
 import android.app.Fragment;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
@@ -18,6 +19,7 @@ import android.widget.Toast;
 import com.example.administrator.coolweather.db.City;
 import com.example.administrator.coolweather.db.County;
 import com.example.administrator.coolweather.db.Province;
+import com.example.administrator.coolweather.gson.Weather;
 import com.example.administrator.coolweather.util.HttpUtil;
 import com.example.administrator.coolweather.util.Utility;
 
@@ -106,6 +108,12 @@ public class ChooseAreaFragment extends Fragment {
                 }else if (currentLevel == LEVEL_CITY){
                     selectedCity = cityList.get(position);
                     queryCounties();
+                }else if (currentLevel ==LEVEL_COUNTY) {
+                    String weatherId = countyList.get(position).getWeatherId();
+                    Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                    intent.putExtra("weather_id", weatherId);
+                    startActivity(intent);
+                    getActivity().finish();
                 }
             }
         });
---
+++ app/src/main/java/com/example/administrator/coolweather/gson/AQI.java
@@ -0,0 +1,20 @@
+package com.example.administrator.coolweather.gson;
+
+import java.security.PublicKey;
+
+/**
+ * Created by Administrator on 2017/12/19 0019.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity{
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+}
