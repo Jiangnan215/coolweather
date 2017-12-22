023125e1a3552c0bf7e0e3b7aa67249fd4ecf25b
jiang <13135672159@163.com>
2017-12-22 09:54:04 +0800

    更新了手动更换城市

--- app/src/main/java/com/example/administrator/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/administrator/coolweather/WeatherActivity.java
@@ -4,11 +4,14 @@ import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
 import android.preference.PreferenceManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
@@ -28,6 +31,14 @@ import okhttp3.Callback;
 import okhttp3.Response;
 
 public class WeatherActivity extends AppCompatActivity {
+
+    //下面是新加入的
+
+    public DrawerLayout drawerLayout;
+
+    private Button navButton;
+
+    /////////////////////////////
 
     public SwipeRefreshLayout swipeRefresh;
 
@@ -82,6 +93,13 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText = (TextView) findViewById(R.id.car_wash_text);
         sportText = (TextView) findViewById(R.id.car_wash_text);
         swipeRefresh = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
+
+        //一下是新加入的
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navButton =(Button) findViewById(R.id.nav_button);
+
+        /////////
         swipeRefresh.setColorSchemeResources(R.color.colorPrimary);
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         String weatherString = prefs.getString("weather",null);
@@ -108,6 +126,17 @@ public class WeatherActivity extends AppCompatActivity {
         }else {
             loadBingPic();
         }
+
+        //一下是新加入的
+
+        navButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        ////////////////////
     }
 
     //根据天气的id请求城市天气信息
