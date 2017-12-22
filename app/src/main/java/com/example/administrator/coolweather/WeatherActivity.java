a9ac996c76441351fa5938105b8b64627f8f2e2e
jiang <13135672159@163.com>
2017-12-22 20:32:52 +0800

    增加了后台自动更新天气的功能和图标名称

--- app/src/main/java/com/example/administrator/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/administrator/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.administrator.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.administrator.coolweather.gson.Forecast;
 import com.example.administrator.coolweather.gson.Weather;
+import com.example.administrator.coolweather.service.AutoUpdateService;
 import com.example.administrator.coolweather.util.HttpUtil;
 import com.example.administrator.coolweather.util.Utility;
 
@@ -32,13 +34,9 @@ import okhttp3.Response;
 
 public class WeatherActivity extends AppCompatActivity {
 
-    //下面是新加入的
-
     public DrawerLayout drawerLayout;
 
     private Button navButton;
-
-    /////////////////////////////
 
     public SwipeRefreshLayout swipeRefresh;
 
@@ -78,7 +76,6 @@ public class WeatherActivity extends AppCompatActivity {
                     View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
             getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
-        setContentView(R.layout.activity_weather);
         //初始化各控件
         bingPicImg = (ImageView) findViewById(R.id.bing_pic_img);
         weatherLayout = (ScrollView) findViewById(R.id.weather_layout);
@@ -93,13 +90,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText = (TextView) findViewById(R.id.car_wash_text);
         sportText = (TextView) findViewById(R.id.car_wash_text);
         swipeRefresh = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
-
-        //一下是新加入的
-
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         navButton =(Button) findViewById(R.id.nav_button);
-
-        /////////
         swipeRefresh.setColorSchemeResources(R.color.colorPrimary);
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         String weatherString = prefs.getString("weather",null);
@@ -126,17 +118,12 @@ public class WeatherActivity extends AppCompatActivity {
         }else {
             loadBingPic();
         }
-
-        //一下是新加入的
-
         navButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 drawerLayout.openDrawer(GravityCompat.START);
             }
         });
-
-        ////////////////////
     }
 
     //根据天气的id请求城市天气信息
@@ -250,5 +237,7 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 }
